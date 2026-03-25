c3f58e7796ce6232530007b034b80d9eeb1463cb
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2026-03-25 20:53:56 +0900

    Add Express server setup to index.js

--- index.js
+++ index.js
@@ -115,4 +115,12 @@ client.on('messageCreate', async (message) => {
   await message.channel.send({ embeds: [embed] });
 });
 
+const express = require("express");
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 client.login(TOKEN);
+
